6988f294c32bc0c6725afac698d3d475e5bbe7bd
Sachin mishra <76901807+sachin-mish@users.noreply.github.com>
2023-07-15 17:48:33 +0530

    Add files via upload

---
+++ ARMSTRONG.cpp
@@ -0,0 +1,41 @@
+// TO FIND ARMSTRONG NUMBER 
+#include <iostream>
+
+#include<math.h>
+
+using namespace std;
+
+int main() {
+  
+ int n;
+
+   cin>>n;
+
+   int sum=0;
+
+   int original=n;
+ 
+  while(n>0){
+     
+ int lastnumber=n%10;
+
+      sum+=pow(lastnumber,3);
+  
+    n=n/10;
+  
+ }
+   if(sum==original){
+
+cout<<"Armstrong number"<<endl;
+
+}
+
+else{
+
+cout<<"Not a Armstrong number";
+ 
+}
+ 
+return 0;
+
+}
